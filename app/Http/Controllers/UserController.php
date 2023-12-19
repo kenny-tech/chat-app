5e11aa6543988a24497ca2cb12892501e4ea40c1
Kenny <kennyemma2008@gmail.com>
2023-12-19 10:14:52 +0100

    Created register and login functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|min:6|confirmed',
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken('authToken')->accessToken;
+
+            return response(['user' => $user, 'access_token' => $token]);
+        } catch (\Exception $e) {
+            return response(['message' => 'Registration failed', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            $credentials = request(['email', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                throw ValidationException::withMessages(['message' => 'Invalid credentials']);
+            }
+
+            $user = $request->user();
+            $token = $user->createToken('authToken')->accessToken;
+
+            return response(['user' => $user, 'access_token' => $token]);
+        } catch (\Exception $e) {
+            return response(['message' => 'Login failed', 'error' => $e->getMessage()], 500);
+        }
+    }
 }
